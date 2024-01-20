4800ff1a4dbd54ecf1cae2a30007644fa248d71f
Andrew Thompson <111794782+Andrewtho5942@users.noreply.github.com>
2024-01-20 11:24:48 -0500

    update settings

---
+++ js/settings.js
@@ -0,0 +1,39 @@
+function createRedirect() {
+    newRedirect = new Redirect();
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("create-btn").addEventListener('click', createRedirect);
