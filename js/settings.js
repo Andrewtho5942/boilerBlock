2b765553473e5884d63e333c6aae254a8a77ffcd
vidit-p <112198253+vidit-p@users.noreply.github.com>
2024-01-20 13:54:31 -0500

    updated settings

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,27 @@
+var elementArray = [];
+
+function updateForms() {
+
+}
+
 function createRedirect() {
     newRedirect = new Redirect();
     //update edit form title
     //el('#edit-redirect-form h3').textContent = 'Create Redirect';
 	//el('#btn-save-redirect').setAttribute('disabled', 'disabled');
+	var sourceURLBox = getElementById("source-from");
+	var sourceURL = sourceURLBox.value;
+	var titleBox = getElementById("title-from");
+	var title = titleBox.value;
+	var whitelistBox = getElementById("whitelist-from");
+	var whitelist = whitelistBox.value;
+	var newRedirect = new Redirect();
+	newRedirect.sourceURL = sourceURL;
+	newRedirect.description = title;
+	newRedirect.whitelist = whitelist;
+	redirects.push(newRedirect);
+	sourceText.value = '';
+
 	
 
 }
