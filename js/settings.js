8c4f881726b57bb6988c2986371992dde1d33404
Andrew Thompson <111794782+Andrewtho5942@users.noreply.github.com>
2024-01-20 13:32:58 -0500

    update settings forms

--- js/settings.js
+++ js/settings.js
@@ -2,17 +2,13 @@ function createRedirect() {
     newRedirect = new Redirect();
     //update edit form title
     //el('#edit-redirect-form h3').textContent = 'Create Redirect';
-	showForm('#edit-redirect-form', activeRedirect);
 	//el('#btn-save-redirect').setAttribute('disabled', 'disabled');
 
 }
 
 function cancelEdit() {
 	activeRedirect = null;
-	hideForm('#edit-redirect-form');
 }
-
-
 
 
 
